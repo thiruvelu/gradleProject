a2ae66483f3b13e5aff21f2adc3e4c9723cfff56
Thiru <thirunavukkarasar.v@gmail.com>
2022-03-04 15:36:06 -0600

    Commit:2

--- src/main/java/HelloGradle.java
+++ src/main/java/HelloGradle.java
@@ -1,5 +1,5 @@
 public class HelloGradle {
     public static void main(String[] args) {
-        System.out.println("Hello Gradle:First Branch: changes from Git");
+        System.out.println("Hello Gradle:2nd Branch: changes from InteliJ");
     }
 }
